3791ce824816cba8e79667fab5b3fe61f5944d81
JinLee <jinlee0908@gmail.com>
2014-02-24 11:38:14 -0800

    updated analyze.rb file

--- txt2csv/lib/analyze.rb
+++ txt2csv/lib/analyze.rb
@@ -7,8 +7,27 @@
 # It reads a text file from STDIN
 # It writes the resulting histogram to STDOUT
 class Analysis
-  def self.histogram(option, txtline, outputtxt)
-    case option
+  def initialize(option, inputtxt, outputtxt)
+    @option = option
+    @inputtxt = inputtxt
+    @outputtxt = outputtxt
+  end
+
+  def process
+    txtline = name_pull
+    regex = pattern
+    histo = histogram(txtline,regex)
+    outputfile = output(histo)
+  end
+
+  def name_pull
+    parsed_names = []
+    IO.foreach(@inputtxt) { |line| parsed_names << line.scan(/[^\t\n]+/)[0] }
+    parsed_names
+  end
+
+  def pattern
+    case @option
     when :prefix
       regular_expression = /^\S*/
     when :suffix
@@ -18,19 +37,21 @@ class Analysis
       puts 'usage: analyze.rb -p | -s < input_file > output_file'
       exit
     end
+  end
 
+  def histogram(txtline,regular_expression)  
     histogram = Hash.new(0)
 
     txtline.each do |name_string|
       word = regular_expression.match(name_string).to_s
       histogram[word.to_sym] += 1
     end
-
-    histogram = Hash[histogram.sort_by { |name, count| count }.reverse]
-
-    File.open(outputtxt, "w+") do |file|
+    Hash[histogram.sort_by { |name, count| count }.reverse]
+  end
+    
+  def output(histogram)
+    File.open(@outputtxt, "w+") do |file|
       histogram.each { |name, count| file.puts "#{name} #{count}" }
     end
-    File.read(outputtxt)
   end
 end
